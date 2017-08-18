bfa8816c170a8d1788996e3fb1ab251ab7daac19
HJ Cross <github@chimpjuice.com>
2017-08-18 18:38:44 -0400

    zoom and pan controls

--- image-maker/index.js
+++ image-maker/index.js
@@ -10,9 +10,8 @@ const port = 3133;
 
 const app = express();
 app.set('port', process.env.PORT || port);
-reload(app, {
-	verbose: true,
-});
+
+app.use('/components', express.static('components'));
 
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '.', 'index.htm'));
@@ -21,6 +20,10 @@ app.get('/', (req, res) => {
 app.use('/css', expressLess(__dirname + '/css', { cache: true }));
 
 app.use('/png', pngroute);
+
+reload(app, {
+	verbose: true,
+});
 
 app.listen(port, () => {
 	console.log(`--- image-maker server running on http://localhost:${app.get('port')}`);
