5610f9bc4c8091c0c93b4230f2a66320078034c2
HJ Cross <github@chimpjuice.com>
2017-08-21 17:47:36 -0400

    client js loads image, better zoom, colors

--- image-maker/routes/png.js
+++ image-maker/routes/png.js
@@ -6,7 +6,10 @@ const PNG = require('node-png').PNG;
 
 routes.get('/', function (req, res) {
 	res.set('Content-Type', 'image/png');
-	const png = new PNG({ width: 1024, height: 768 });
+	const png = new PNG({
+    width: req.query.width || 1024,
+    height: req.query.height || 768
+  });
 
   var magnificationFactor = req.query.mag;
   var panX = req.query.panx;
@@ -15,7 +18,6 @@ routes.get('/', function (req, res) {
 	for (var y = 0; y < png.height; y++) {
 		for (var x = 0; x < png.width; x++) {
 			var idx = (png.width * y + x) << 2;
-			// TODO: pass these params to png
 			var ex = x / magnificationFactor - panX;
 			var ay = y / magnificationFactor - panY;
 			var inSet = checkIfBelongsToMandelbrotSet(ex, ay);
@@ -25,7 +27,7 @@ routes.get('/', function (req, res) {
 				png.data[idx + 2] = 0;
 				png.data[idx + 3] = 255;
 			} else {
-				var rgb = hslToRgb(0, 1, inSet);
+				var rgb = hslToRgb.apply(null, hslFromInset(inSet));
 				png.data[idx] = rgb[0];
 				png.data[idx + 1] = rgb[1];
 				png.data[idx + 2] = rgb[2];
@@ -36,11 +38,25 @@ routes.get('/', function (req, res) {
 	png.pack().pipe(res);
 });
 
+function hslFromInset(inSet){
+  // return [
+  //   Math.sin(inSet < .4 ? .4-inSet : inSet)+.5,
+  //   inSet < .4 ? .6-inSet : inSet,
+  //   inSet > .4
+  //     ? inSet * 1.2
+  //     : inSet / 1.2
+  // ];
+  return [
+    1-((inSet/5)+.25),
+    inSet, //inSet < .4 ? .5-inSet : inSet,
+    inSet*.9
+  ];
+}
 
 function checkIfBelongsToMandelbrotSet(x, y) {
 	var realComponentOfResult = x;
 	var imaginaryComponentOfResult = y;
-	var maxIterations = 75;
+	var maxIterations = 900;
 	for (var i = 0; i < maxIterations; i++) {
 		var tempRealComponent = realComponentOfResult * realComponentOfResult - imaginaryComponentOfResult * imaginaryComponentOfResult + x;
 		var tempImaginaryComponent = 2 * realComponentOfResult * imaginaryComponentOfResult + y;
--- image-maker/static/client.js
+++ image-maker/static/client.js
@@ -3,30 +3,50 @@
 
 function receiveMessage(event){
   const image = document.getElementById('main_image');
+
+  //./png?mag=348040563.5232469&panx=1.9854872904239196&pany=-0.000014568622124230988&width=1920&height=1080
+  if(event.data === 'init'){
+    image.src = "./png?" +
+      "mag=348040563.5232469&" + //"mag=374606.88409205293&" +
+      "panx=1.9854867732428236&" +//"panx=1.77402235874246&" +
+      "pany=-0.000014568622124230988&" +//"pany=-0.005048224154787757&" +
+      "width=" + document.body.scrollWidth + "&" +
+      "height=" + document.body.scrollHeight;
+    return;
+  }
+
   const newVals = {
     mag: Number(image.src.match(/mag=([^&]*)/)[1]),
     panx: Number(image.src.match(/panx=([^&]*)/)[1]),
     pany: Number(image.src.match(/pany=([^&]*)/)[1])
   };
+  var panAmount = 0.1 * (600/newVals.mag);
+  var magAmount = 100 * (newVals.mag/600);
   if(event.data === 'north'){
-    newVals.pany = newVals.pany - 0.1;
+    newVals.pany = newVals.pany + panAmount;
   }
   if(event.data === 'south'){
-    newVals.pany = newVals.pany + 0.1;
+    newVals.pany = newVals.pany - panAmount;
   }
   if(event.data === 'east'){
-    newVals.panx = newVals.panx + 0.1;
+    newVals.panx = newVals.panx - panAmount;
   }
   if(event.data === 'west'){
-    newVals.panx = newVals.panx - 0.1;
+    newVals.panx = newVals.panx + panAmount;
   }
   if(event.data === 'plus'){
-    newVals.mag = newVals.mag + 100;
+    newVals.mag = newVals.mag + magAmount;
+    newVals.panx = newVals.panx - panAmount*1.25;
+    newVals.pany = newVals.pany - panAmount*1.25;
   }
   if(event.data === 'minus'){
-    newVals.mag = newVals.mag - 100;
+    newVals.mag = newVals.mag - magAmount;
+    newVals.panx = newVals.panx + panAmount*1.75;
+    newVals.pany = newVals.pany + panAmount*1.75;
   }
-  const newSrc = `./png?mag=${newVals.mag}&panx=${newVals.panx}&pany=${newVals.pany}` + '';
+  const newSrc = `./png?mag=${newVals.mag}&panx=${newVals.panx}&pany=${newVals.pany}&` +
+    "width=" + document.body.scrollWidth + "&" +
+    "height=" + document.body.scrollHeight;
   //console.log({newVals, newSrc});
   image.src = newSrc;
   //console.log('Event from child:', event.data);
