7a43aecf9a09ab329948cdc2d6c8009166d28748
crosshj <github@chimpjuice.com>
2017-09-22 18:07:23 -0400

    Update drive-window.js

--- drive-window/drive-window.js
+++ drive-window/drive-window.js
@@ -7,9 +7,16 @@ deev.innerHTML=`
 	This demo, the parent window drives
 `;
 foo.document.body.appendChild(deev);
+foo.document.title = "drive-window-child";
 
 var scree = foo.document.createElement('script');
 scree.text=`
+	var deev = document.createElement('div');
+	deev.innerHTML=`
+		<img alt="" class="avatar width-full rounded-2" height="230" src="https://avatars0.githubusercontent.com/u/1816471?v=4&amp;s=460" width="230">
+		This demo, the parent window drives
+	`;
+	document.body.appendChild(deev);
 	fetch(\`\${document.querySelectorAll('img')[0].src}\`)
 		.then((res)=>{
 			if(!res){ return console.log("res not defined"); }
