c3fd7c9e4be28d20b1ddd4ac9401d234546fbd9a
crosshj <github@chimpjuice.com>
2017-09-22 18:14:56 -0400

    Update drive-window.js

--- drive-window/drive-window.js
+++ drive-window/drive-window.js
@@ -4,7 +4,7 @@ var foo = window.open("about:blank", "newWindowName", null, false);
 setTimeout(()=>{
 	var deev = foo.document.createElement('div')
 	deev.innerHTML=`
-		<img alt="" class="avatar width-full rounded-0" height="130" src="https://avatars0.githubusercontent.com/u/1816471?v=4&amp;s=460" width="230">
+		<img alt="" class="avatar width-full rounded-0" height="230" src="https://avatars0.githubusercontent.com/u/1816471?v=4&amp;s=460" width="230">
 		<p>This demo, the parent window drives</p>
 	`;
 	foo.document.body.appendChild(deev);
@@ -15,7 +15,7 @@ var scree = foo.document.createElement('script');
 scree.text=`
 	var deev = document.createElement('div');
 	deev.innerHTML=\`
-		<img alt="" class="avatar width-full rounded-2" height="230" src="https://avatars0.githubusercontent.com/u/1816471?v=4&amp;s=460" width="230">
+		<img alt="" class="avatar width-full rounded-2" height="130" width="130" src="https://avatars0.githubusercontent.com/u/1816471?v=4&amp;s=460" width="230">
 		This demo, the parent window drives
 	\`;
 	document.body.appendChild(deev);
