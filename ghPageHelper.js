ee5359fc1a9ed42b5e3d53cb9960a2cc1b6815dc
Cross, Harrison (CORP) <Harrison.Cross@adp.com>
2017-11-24 11:21:36 -0500

    append CSS, for ghpagehelper

--- ghPageHelper.js
+++ ghPageHelper.js
@@ -30,7 +30,82 @@ window.ghPageHelper = (function () {
           ul.appendChild(li);
         });
         rootEl.appendChild(ul);
-      })
+      });
+
+    // append CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = `
+      body {
+        font-family: monospace;
+        font-size: smaller;
+      }
+      
+      #projects {
+        width: 100%;
+        margin: 0px auto;
+      }
+      
+      #projects ul {
+        list-style-type: none;
+        padding-left: 0px;
+      }
+      
+      #projects li {
+        margin-bottom: 10px;
+        padding-bottom: 5px;
+        padding-left: 5px;
+      }
+      
+      #projects li:hover {
+        background-color: aquamarine;
+        cursor: default;
+      }
+      
+      #projects li a {
+        float: right;
+        margin: 0px 5px;
+        text-decoration: none;
+      }
+      
+      #projects li a:hover {
+        background-color: black;
+        color: white;
+        text-decoration: none;
+      }
+      
+      
+      @media (min-width:320px) {
+        /* smartphones, portrait iPhone, portrait 480x320 phones (Android) */
+        #projects {
+          font-size: small;
+          width: 90%;
+        }
+      }
+      @media (min-width:480px) {
+        /* smartphones, Android phones, landscape iPhone */
+        #projects {
+          width: 70%;
+          font-size: large;
+        }
+      }
+      @media (min-width:600px) {
+        /* portrait tablets, portrait iPad, e-readers (Nook/Kindle), landscape 800x480 phones (Android) */
+        #projects {
+          width: 60%;
+        }
+      }
+      @media (min-width:801px) {
+        /* tablet, landscape iPad, lo-res laptops ands desktops */
+        #projects {
+          width: 45%;
+          font-size: larger;
+        }
+      }
+      @media (min-width:1025px) { /* big landscape tablets, laptops, and desktops */ }
+      @media (min-width:1281px) { /* hi-res laptops and desktops */ }
+    `;
+    document.body.appendChild(css);
   }
 
   var helper = { appendChildLinks };
