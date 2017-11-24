6bc2edf840e82d593c144bb871155763ec95bf37
HJ Cross <github@chimpjuice.com>
2017-11-24 08:35:22 -0500

    experiments page helper

---
+++ ghPageHelper.js
@@ -0,0 +1,36 @@
+window.ghPageHelper = (function () {
+
+  //usage: appendChildLinks('#projects', 'crosshj') 
+  function appendChildLinks(rootSelector, username) {
+    const site = document.location.origin;
+    const source = `https://github.com/${username}`;
+    const root = document.location.pathname;
+
+    fetch(`https://api.github.com/repos/${username}${pathname}contents/src?ref=gh-pages`)
+      .then(res => res.json())
+      .then(json => {
+        console.table(json);
+
+        const rootEl = document.querySelector(rootSelector);
+        var ul = document.createElement('ul');
+        json.forEach(j => {
+          const li = document.createElement('li');
+          li.textContent = j.name;
+          [{ source }, { site }].forEach(base => {
+            const a = document.createElement('a');
+            a.className = base.site ? 'demo' : 'source'
+            a.href = 'https://' + (base.site || base.source) + root + (base.source ? 'tree/gh-pages/' : '') + j.path;
+            a.textContent = base.site ? '[demo]' : '[source]';
+            li.appendChild(a);
+          });
+
+          ul.appendChild(li);
+        });
+        rootEl.appendChild(ul);
+      })
+  }
+
+  var helper = { appendChildLinks };
+  return helper;
+
+}());
