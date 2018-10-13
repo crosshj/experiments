01e866109d419a8020b70e7aca4aca171dbd749a
HJ Cross <github@chimpjuice.com>
2018-10-12 23:59:58 -0400

    websocket/reload for local dev, new data

--- rangers/most_used.js
+++ rangers/most_used.js
@@ -1,58 +1,89 @@
 var columnify = require('columnify')
-var {
+var fetch = require('./data');
+
+function mostUsed({
   pvp, rangers, translateWords
-} = require('./data');
+}) {
+  var foo = pvp.playerInfo.reduce((all, item) => {
+    if (!item.playerUnitPvPTeams && !item.playerUnitTeams){
+      return all;
+    }
+    const team1 =
+      item.playerUnitPvPTeams['1'] ||
+      item.playerUnitTeams['1'] ||
+      [];
+    team1.forEach((i) => {
+      all.push(i);
+    });
 
-var foo = pvp.playerInfo.reduce((all, item) => {
+    const team2 = item.playerUnitPvPTeams['1'] ||
+      item.playerUnitTeams['2'] ||
+      [];
+    team2.forEach((i) => {
+      all.push(i);
+    });
 
-  item.playerUnitTeams['1'].forEach((i) => {
-    all.push(i);
+    return all;
+
+  }, []);
+
+  var fee = foo.reduce((all, i) => {
+    if (!all[i.unitCode]) {
+      all[i.unitCode] = [];
+    }
+    all[i.unitCode].push(i);
+
+    return all;
+  }, {});
+
+  var fow = Object.keys(fee).sort(function (a, b) {
+    return fee[b].length - fee[a].length;
+  }).map(x => {
+    var obj = {};
+    //console.log(translateWords['en-UNIT'][x + '_nm'])
+    obj.name = translateWords['en-UNIT'][x + '_nm'];
+    obj.levels = fee[x];
+    return obj;
   });
 
-  item.playerUnitTeams['2'].forEach((i) => {
-    all.push(i);
-  });
+  var data = [];
+  var options = { minWidth: 10 };
+  fow
+    //.filter((x, i) => i < 30)
+    .forEach(x => {
+      var levels = x.levels.map(y => y.unitLevel);
+      var average = levels.reduce((all, i) => all + i) / levels.length
 
-  return all;
+      data.push({ name: x.name, average: average.toFixed(2), count: levels.length })
+      //console.log(x.name, ' :\t\t', average.toFixed(2))
+    });
+  var columns = columnify(data, options)
 
-}, []);
+  //console.log('this');
 
-var fee = foo.reduce((all, i) => {
-  if (!all[i.unitCode]) {
-    all[i.unitCode] = [];
-  }
-  all[i.unitCode].push(i);
-
-  return all;
-}, {});
-
-var fow = Object.keys(fee).sort(function(a, b) {
-  return fee[b].length - fee[a].length;
-}).map(x => {
-	var obj = {};
-	//console.log(translateWords['en-UNIT'][x + '_nm'])
-	obj.name = translateWords['en-UNIT'][x + '_nm'];
-  obj.levels = fee[x];
-	return obj;
-});
-
-var data=[];
-var options = { minWidth: 10};
-fow
-//.filter((x, i) => i < 30)
-.forEach(x=>{
-  var levels = x.levels.map(y=>y.unitLevel);
-  var average = levels.reduce((all, i) => all+i) / levels.length
-
-  data.push({name: x.name, average: average.toFixed(2), count: levels.length})
-  //console.log(x.name, ' :\t\t', average.toFixed(2))
-});
-var columns = columnify(data, options)
-console.log(`
+  return `
 <pre>
-Most Used Rangers (NOT LIVE DATA)
+Most Used Rangers
 -----------------------------------------
 ${columns}
 </pre>
-`);
-//console.log(JSON.stringify(fow[0], null, '\t'));
+`;
+  //console.log(JSON.stringify(fow[0], null, '\t'));
+}
+
+const getMostUsed = (callback) => {
+  fetch((err, {
+    pvp, rangers, translateWords
+  } = {}) => {
+    callback( err, mostUsed({
+        pvp, rangers, translateWords
+      })
+    )
+  });
+};
+
+module.exports = getMostUsed;
+
+if (!module.parent) {
+    getMostUsed((err, data) => console.log(err || data));
+}
