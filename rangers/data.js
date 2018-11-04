4a26318dd843cf969e22b0bd7bce16d87303fb16
HJ Cross <github@chimpjuice.com>
2018-11-03 20:41:20 -0400

    rangers: gear analytics, harder cache strat

--- rangers/data.js
+++ rangers/data.js
@@ -1,5 +1,6 @@
 
 var async = require('async');
+const fs = require('fs');
 var request = require('request')
 var cachedRequest = require('cached-request')(request)
 var cacheDirectory = "./.cache";
@@ -8,7 +9,22 @@ cachedRequest.setCacheDirectory(cacheDirectory);
 const ONE_HOUR = 60 /*minutes*/ * 60 /*seconds*/ * 1000 /*milliseconds*/;
 cachedRequest.setValue('ttl', ONE_HOUR);
 
-var pvp, rangers, translateWords;
+const HARD_CACHE_JSON = './.cache/hardCache.json';
+const hardCache = ensureRequireJSON(HARD_CACHE_JSON) || {};
+
+const logNice = (item) => {
+  console.log(JSON.stringify(item, null, '\t'));
+};
+
+function ensureRequireJSON(path){
+  let json;
+  try {
+    json = require(path);
+  } catch(e) {
+    fs.openSync(path, 'w');
+  }
+  return json;
+};
 
 function tryParse(input){
   var output;
@@ -18,31 +34,84 @@ function tryParse(input){
   return output;
 }
 
+let cachedRequests = 0;
+let fetchedRequests = 0;
 function requestUrl(url, callback){
+  if(hardCache[url]){
+    //console.log(`CACHED: ${url}`);
+    cachedRequests++;
+    return callback(undefined, hardCache[url]);
+  }
+  //console.log(`FETCH: ${url}`)
+  fetchedRequests++;
   var options = { url };
-  cachedRequest(options, (e,a,t) => callback(e, tryParse(t)) );
+  cachedRequest(options, (e,a,t) => {
+    const results = tryParse(t);
+    if(results){
+      hardCache[url] = results;
+      fs.writeFileSync(
+        HARD_CACHE_JSON,
+        JSON.stringify(hardCache, null, '\t')
+      );
+    }
+    callback(e, results)
+  });
 }
 
-// NOTE: this is needed in order to track gear
-function getPlayerInfo(playerId, callback){
+//NOTE: this is needed in order to track top gear usage
+function getPlayerInfo(item, callback){
   // https://rangers.lerico.net/api/getPlayer/u610d9edfeaf01bdb67f66a4a1d02317f
+  const playerId = item;
   const playerUrl = `https://rangers.lerico.net/api/getPlayer/${playerId}`;
+  requestUrl(playerUrl, callback);
+}
 
-  // TODO: fetch url and return results
+function quickSave(data, path){
+  fs.writeFileSync(path, JSON.stringify(data, null, '\t'));
+}
+
+function overlayFullPlayerInfo(pvp, allPlayerInfo){
+  const mapped = JSON.parse(JSON.stringify(pvp));
+  //console.log(Object.keys(pvp));
+  // console.log(JSON.stringify(pvp.playerInfo[1].playerUnitPvPTeams, null, '\t'));
+  // console.log(JSON.stringify({
+  //   error,
+  //   result: allPlayerInfo[0].playerUnitPvPTeams
+  // }, null, '\t'));
+  // logNice({
+  //   original: mapped.playerInfo[1],
+  //   updated: allPlayerInfo[1]
+  // }, './.foo.json');
+  pvp.playerInfo = allPlayerInfo;
+  return pvp;
+}
+
+function getAllPlayerInfo(pvp, callback){
+  // all players in series, get player info and add to pvp
+  const allPlayerIds = pvp.playerInfo.map(x => x.mid);
+  async.mapSeries(allPlayerIds, getPlayerInfo, (error, results) => {
+    const overlayed = overlayFullPlayerInfo(pvp, results);
+    callback(null, overlayed);
+  });
 }
 
 function fetch(callback){
   const urls = [
     'https://rangers.lerico.net/api/getPvps',
     'https://rangers.lerico.net/api/getRangersBasics',
-    'https://rangers.lerico.net/api/translate/en/UNIT'
+    'https://rangers.lerico.net/api/translate/en/UNIT',
+    'https://rangers.lerico.net/api/v2/stages/advent',
+    'https://rangers.lerico.net/api/v2/stages/normal',
+    'https://rangers.lerico.net/api/v2/stages/special',
+    'https://rangers.lerico.net/api/getEquipments',
+    'https://rangers.lerico.net/api/getEquipmentAttrs'
   ];
   async.map(urls, requestUrl, function(err, results) {
       if(err) {
         return callback(err);
       }
-      pvp = results[0];
-      rangers = results[1];
+      const pvp = results[0];
+      const rangers = results[1];
       if(!results[2]['en-UNIT']){
         translateWords = {
           'en-UNIT': results[2]
@@ -50,7 +119,21 @@ function fetch(callback){
       } else {
         translateWords = results[2];
       }
-      callback(undefined, { pvp, rangers, translateWords });
+      const stages = {
+        advent: results[3],
+        normal: results[4],
+        special: results[5],
+      };
+      const gear = results[6];
+      const gearAttrs = results[7];
+      getAllPlayerInfo(pvp, (e, p) => {
+        if(e){
+          return callback(e);
+        }
+        callback(undefined, {
+          pvp:p, rangers, translateWords, stages, gear, gearAttrs
+        });
+      });
   });
 }
 
@@ -58,12 +141,23 @@ module.exports = fetch;
 
 if(!module.parent){
   fetch((err, data) => {
+    const {
+      pvp, rangers, translateWords, stages, gear, gearAttrs
+    } = data;
+
     console.log(err || {
-      pvp: data.pvp.top100.length,
-      rangers: data.rangers.length,
+      pvp: pvp.top100.length,
+      rangers: rangers.length,
       words: Object.keys(
-        data.translateWords['en-UNIT']
-      ).length
+        translateWords['en-UNIT']
+      ).length,
+      advent: stages.advent.length,
+      normal: stages.normal.length,
+      special: stages.special.length,
+      gear: gear.length,
+      gearAttrs: gearAttrs.length,
+      cachedRequests,
+      fetchedRequests
     })
   });
 }
--- rangers/most_used.js
+++ rangers/most_used.js
@@ -23,79 +23,29 @@ ITERATE:
 */
 
 
+const propsMap = (object, func) => Object.entries(object)
+  .map(func)
+  .reduce((all, one) => {
+    return { ...all, ...{ [one[0]]: one[1] } };
+  }, {});
+
+
 var columnify = require('columnify')
 var fetch = require('./data');
 
-function mostUsed({
-  pvp, rangers, translateWords, dataOnly
-}) {
-  var foo = pvp.playerInfo.reduce((all, item) => {
-    if (!item.playerUnitPvPTeams && !item.playerUnitTeams){
-      return all;
-    }
-    const team1 =
-      item.playerUnitPvPTeams['1'] ||
-      item.playerUnitTeams['1'] ||
-      [];
-    team1.forEach((i) => {
-      all.push(i);
-    });
-
-    const team2 = item.playerUnitPvPTeams['1'] ||
-      item.playerUnitTeams['2'] ||
-      [];
-    team2.forEach((i) => {
-      all.push(i);
-    });
-
-    return all;
-
-  }, []);
-
-  var fee = foo.reduce((all, i) => {
-    if (!all[i.unitCode]) {
-      all[i.unitCode] = [];
-    }
-    all[i.unitCode].push(i);
-
-    return all;
-  }, {});
-
-  var fow = Object.keys(fee).sort(function (a, b) {
-    return fee[b].length - fee[a].length;
-  }).map(x => {
-    var obj = {};
-    //console.log(translateWords['en-UNIT'][x + '_nm'])
-    obj.name = translateWords['en-UNIT'][x + '_nm'];
-    obj.levels = fee[x];
-    return obj;
-  });
-
-  var data = [];
+function formattedResults(rangers, translateWords, data) {
   var options = { minWidth: 10 };
-  fow
-    //.filter((x, i) => i < 30)
-    .forEach(x => {
-      var levels = x.levels.map(y => y.unitLevel);
-      var average = levels.reduce((all, i) => all + i) / levels.length
 
-      data.push({ name: x.name, average: average.toFixed(2), count: levels.length })
-      //console.log(x.name, ' :\t\t', average.toFixed(2))
-    });
-
-
-  var safeRangers = rangers.filter(x =>
-    x.unitCode[0] === 'u' &&
+  var safeRangers = rangers.filter(x => x.unitCode[0] === 'u' &&
     x.unitCategoryType !== 'INT' &&
     translateWords['en-UNIT'][x.unitNameCode] &&
-    x.created
-  );
+    x.created);
 
   function topByProp(p, number, full) {
-    var sortDesc = (prop) => safeRangers.sort((a,b) => b[prop] - a[prop]);
+    var sortDesc = (prop) => safeRangers.sort((a, b) => b[prop] - a[prop]);
     var initHPsorted = sortDesc(p);
     var initialHpTop = [];
-    (new Array(number)).fill().forEach((a, i) => {
+    (new Array(number)).fill().forEach((_a, i) => {
       var x = initHPsorted[i];
       var min = {
         name: translateWords['en-UNIT'][x.unitNameCode],
@@ -103,44 +53,32 @@ function mostUsed({
       };
       initialHpTop.push(!full
         ? min
-        : Object.assign({}, x, min)
-      );
+        : Object.assign({}, x, min));
     });
     return initialHpTop;
   }
-
-
   const lister = (long, prop) => {
     return `
 Top Rangers by ${long}
 -----------------------------------------
-${columnify(topByProp(prop, 30),
-  {config: {[prop]: {align: 'right'}}}
-)}`;
+${columnify(topByProp(prop, 30), { config: { [prop]: { align: 'right' } } })}`;
   };
-
-
   // https://gist.github.com/jonlabelle/5375315
-  function lowerCase(str){
+  function lowerCase(str) {
     return str.toLowerCase();
   }
-
-  function upperCase(str){
+  function upperCase(str) {
     return str.toUpperCase();
   }
-
-  function unCamelCase(str){
+  function unCamelCase(str) {
     str = str.replace(/([a-z\xE0-\xFF])([A-Z\xC0\xDF])/g, '$1 $2');
     str = str.toLowerCase(); //add space between camelCase text
     return str;
   }
-
-  function properCase(str){
+  function properCase(str) {
     return lowerCase(str).replace(/^\w|\s\w/g, upperCase);
   }
-
   const camelBreak = (str) => properCase(unCamelCase(str));
-
   const stats = [
     'initialHp',
     'hpIncreaseAmount',
@@ -152,16 +90,10 @@ ${columnify(topByProp(prop, 30),
     'summonEnergy',
     'criticalProbability'
   ].map(x => {
-    return lister(camelBreak(x), x)
-  }).join('\n')
-
-  if(dataOnly){
-    return data;
-  }
-
+    return lister(camelBreak(x), x);
+  }).join('\n');
   //console.log(JSON.stringify(fow[0], null, '\t'));
   console.log(Object.keys(topByProp('initialAttack', 3, true)[0]).join(', '));
-
   return `
 <pre>
 
@@ -175,13 +107,153 @@ ${stats}
 `;
 }
 
-const getMostUsed = (callback, { dataOnly } ) => {
+function getAllTeamMembers(pvp){
+  const all = pvp.playerInfo.reduce((all, item) => {
+    if (!item.playerUnitPvPTeams && !item.playerUnitTeams) {
+      return all;
+    }
+    const team1 = item.playerUnitPvPTeams['1'] || [];
+    team1.forEach((i) => {
+      all.push(i);
+    });
+    const team2 = item.playerUnitPvPTeams['1'] || [];
+    team2.forEach((i) => {
+      all.push(i);
+    });
+    return all;
+  }, []);
+  return all;
+}
+
+function topRangers(pvp, translateWords) {
+
+  var allTeamMembers = getAllTeamMembers(pvp);
+
+  var membersByRanger = allTeamMembers.reduce((all, i) => {
+    if (!all[i.unitCode]) {
+      all[i.unitCode] = [];
+    }
+    all[i.unitCode].push(i);
+    return all;
+  }, {});
+
+  var rangersWithCount = Object.keys(membersByRanger)
+    .sort(function (a, b) {
+      return membersByRanger[b].length - membersByRanger[a].length;
+    })
+    .map(x => {
+      var obj = {};
+      //console.log(translateWords['en-UNIT'][x + '_nm'])
+      obj.name = translateWords['en-UNIT'][x + '_nm'];
+
+      var levels = membersByRanger[x].map(y => y.unitLevel);
+      var average = levels.reduce((all, i) => all + i) / levels.length;
+
+      //obj.levels = levels;
+      obj.average = average.toFixed(2);
+      obj.count = levels.length
+      return obj;
+    });
+
+  //console.log(rangersWithCount.length);
+  return { rangers: rangersWithCount };
+}
+
+const uniqueWithCapture = (collection, uniquePropName) => {
+  const results = collection.reduce((all, one) => {
+    const propValue = one[uniquePropName];
+    //console.log(propValue);
+    if(!all[propValue]){
+      all[propValue] = [];
+    }
+    all[propValue].push(one);
+    return all;
+  }, {});
+  //console.log(Object.keys(results));
+  const interim = Object.entries(results)
+    .map(x => {
+      const key = x[0];
+      const value = x[1];
+      return {
+        [uniquePropName]: key,
+        length: value.length
+      }
+    })
+    .sort((a,b) => b.length - a.length);
+
+  // NOTE: called interim because would prefer to flatten props
+  return interim;
+}
+
+function uniqueGear(gearOfType){
+  const uniqueGearOfType = [
+    gearOfType[0],
+    uniqueWithCapture(gearOfType[1], 'itemCode'),
+  ];
+  return uniqueGearOfType;
+}
+
+// function translateAllGearName(gearOfType){
+//   const translateGearName = (gear) => {
+//     const translated =
+//   };
+//   const transGearOfType = [
+//     gearOfType[0],
+//     translateGearName(gearOfType[1]),
+//   ];
+//   return transGearOfType;
+// }
+
+function topGear(pvp, translateWords){
+  var allTeamMembers = getAllTeamMembers(pvp);
+
+  var allGear = allTeamMembers
+    .map(x => x.equipMap)
+    .filter(x => !!x);
+  //console.log(allTeamMembers[0]);
+
+  var allWeapon = allGear.map(x => x.WEAPON);
+  var allAcc = allGear.map(x => x.ACC);
+  var allArmor = allGear.map(x => x.ARMOR);
+
+  const unique = propsMap({allWeapon, allAcc, allArmor}, uniqueGear);
+  //const uniqueTranslated = propsMap(unique, translateAllGearName);
+  //console.log(unique);
+
+  return unique;
+}
+
+function mostUsed({
+  pvp, rangers, translateWords, dataOnly, full
+}) {
+  const { rangers: _rangers } = topRangers(pvp, translateWords);
+  const gear = topGear(pvp, translateWords);
+
+  if (dataOnly && !full) {
+    return _rangers;
+  }
+
+  if (dataOnly && full) {
+    return {
+      rangers: _rangers.length,
+      gear: {
+        weapons: gear.allWeapon,
+        acc: gear.allAcc,
+        armor: gear.allArmor
+      }
+    }
+  }
+
+  return formattedResults(rangers, translateWords, _rangers);
+}
+
+const getMostUsed = (callback, { dataOnly, full } = {}) => {
   fetch((err, {
     pvp, rangers, translateWords
   } = {}) => {
-    callback( err, mostUsed({
-        pvp, rangers, translateWords, dataOnly
-      })
+    callback(err, mostUsed({
+      pvp, rangers, translateWords, dataOnly, full
+    })
     )
   });
 };
@@ -189,5 +261,11 @@ const getMostUsed = (callback, { dataOnly } ) => {
 module.exports = getMostUsed;
 
 if (!module.parent) {
-    getMostUsed((err, data) => console.log(err || data));
+  const dataOnly = true;
+  const full = true;
+  getMostUsed(
+    (err, data) => console.log(dataOnly ? JSON.stringify({ err, data }, null, '\t') : err || data),
+    { dataOnly, full }
+  );
 }
+
