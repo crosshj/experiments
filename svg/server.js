4df2c0e65edf37c67905320e0620b62a3262130d
HJ Cross <github@chimpjuice.com>
2019-03-30 14:25:06 -0400

    svg: working on engine build system

--- svg/server.js
+++ svg/server.js
@@ -3,6 +3,8 @@ const app = express();
 const dot = require("dot");
 const fs = require('fs');
 const path = require('path');
+const { join } = require('path');
+
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 9090 });
 
@@ -44,19 +46,46 @@ Object.keys(replace).forEach(key => {
     templateFromHTML = templateFromHTML.replace(replace[key], `{{=it.${key}}}`);
 });
 
+var engineSrc;
 const render = (callback) => {
+    if(engineSrc){
+        const templateRender = dot.template(
+            templateFromHTML
+        )(files({ engine: engineSrc }));
+        callback(null, templateRender);
+        return;
+    }
+    const engineSrcPath = join(__dirname, "./engine-src/index.js");
+    const engineBldPath = join(__dirname, "./engine.js");
+
+    //TODO: if engine.js and engine-src are "same", use engine.js
+    // ^^^ and this should be built into buildEngine (not here)
+
+    // const stats = fs.statSync(filename);
+    // if (stats.mtime.valueOf() === previousMTime.valueOf()) {
+    //   return;
+    // }
+
     buildEngine((err, data) => {
         if(err){
             callback(JSON.stringify(err));
         }
-        const engine = `/*! Expression Engine */\n${data['expressionEngine.js']}`;
-        const fs = require('fs');
-        fs.writeFile("./engine.js", engine, function(err) {
+        engineSrc = `/*! Expression Engine */\n${data['expressionEngine.js']}`;
+        const engine = engineSrc;
+
+        fs.writeFile(engineBldPath, engine, function(err) {
             if(err) {
                 return console.log(err);
             }
+            const sourceStats = fs.statSync(engineBldPath);
+            const buildStats = fs.statSync(engineSrcPath);
+            console.log({
+                sourceStats, buildStats
+            });
+            // set file time
+            //fs.utimesSync(path, atime, mtime)
 
-            console.log("The file was saved!");
+            console.log("engine.js built");
         });
         const templateRender = dot.template(
             templateFromHTML
