ab78cd4bcb3eb95e96a79ed727b968a5c1909182
HJ Cross <github@chimpjuice.com>
2019-04-16 00:42:40 -0400

    Update expressionEngine.js
    notes about scripts talking to each other and UI

--- svg/engine-src/expressionEngine.js
+++ svg/engine-src/expressionEngine.js
@@ -370,6 +370,10 @@ function Environment({ units = [], links = [] }) {
     })();
 
     function _on(context, key, callback) {
+        //TODO: if send/ack from one compiled script then
+        //  - activate link (send message to UI for link)
+        //  - notify other compiled script (handler) (should have listeners for this?)
+        //  - ...
         if (context.eventListeners[key] === undefined) {
             context.eventListeners[key] = [];
         }
