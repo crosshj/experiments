12037d45023a3cc8926dff5becceae65eceef280
Harrison Cross <harrison.cross@adp.com>
2019-10-18 19:24:33 -0400

    template-app: responsive sidenav, gutter resize cursor fix, codemirro indent

--- template-app/index.js
+++ template-app/index.js
@@ -4,17 +4,34 @@
 const editor = CodeMirror.fromTextArea(document.querySelector('.simulation .functionInput'), {
 	lineNumbers: true,
   mode:  "javascript",
-	theme: 'bespin',
+	//theme: 'bespin',
 	styleActiveLine: true,
 	matchBrackets: true
 });
+
+CodeMirror.keyMap.default["Shift-Tab"] = "indentLess";
+CodeMirror.keyMap.default["Tab"] = "indentMore";
 
 editor.getDoc().setValue((new Array(300)).fill('code goes here').join('\n'));
 
 
 Split({
+	rowCursor: 'ns-resize',
   rowGutters: [{
     track: 2,
     element: document.querySelector('.gutter-footer'),
-  }]
-})
+	}],
+	onDragStart: () => {
+		document.documentElement.classList.add("ns-draggable-cursor");
+	},
+	onDragEnd: () => {
+		document.documentElement.classList.remove("ns-draggable-cursor");
+	}
+});
+
+function setupSideNav(){
+	var elems = document.querySelectorAll('.sidenav');
+	var options = {};
+	var instances = M.Sidenav.init(elems, options);
+}
+setupSideNav();
