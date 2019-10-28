33d5742507f17376ac4e7d959cfc4a171804f940
HJ Cross <github@chimpjuice.com>
2019-10-28 19:13:14 -0400

    app_hub: init idea

---
+++ app_hub/ideal_poj/index.js
@@ -0,0 +1,3 @@
+import Module from './modules/module.mjs';
+
+window.ExampleModule;
---
+++ app_hub/ideal_poj/modules/module.mjs
@@ -0,0 +1,8 @@
+const setupModule = () => {
+	const example_module = {
+		test: 'foo'
+	};
+	return example_module;
+};
+
+export default setupModule();
