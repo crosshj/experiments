27665095879e565b3f7319ef49d1fad8933099d8
crosshj <github@chimpjuice.com>
2019-11-02 23:05:25 -0400

    template-app: favicon fix, editor to module

--- template-app/index.js
+++ template-app/index.js
@@ -1,24 +1,14 @@
 import './modules/notes.mjs';
 import './modules/lorum.mjs';
+
+import Editor from './modules/editor.mjs';
+window.Editor = Editor;
+
 import theme from './modules/theme.mjs';
-theme({
+theme && theme({
 	mainColor: document.querySelector('meta[name="theme-color"]').content
 });
 
-
-
-const editor = CodeMirror.fromTextArea(document.querySelector('.simulation .functionInput'), {
-	lineNumbers: true,
-  mode:  "javascript",
-	//theme: 'bespin',
-	styleActiveLine: true,
-	matchBrackets: true
-});
-
-CodeMirror.keyMap.default["Shift-Tab"] = "indentLess";
-CodeMirror.keyMap.default["Tab"] = "indentMore";
-
-editor.getDoc().setValue((new Array(300)).fill('code goes here').join('\n'));
 
 
 Split({
@@ -42,55 +32,52 @@ function setupSideNav(){
 }
 setupSideNav();
 
+/* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.documentElement;
 
+/* View in fullscreen */
+function openFullscreen(e) {
+	if (elem.requestFullscreen) {
+		elem.requestFullscreen();
+	} else if (elem.mozRequestFullScreen) { /* Firefox */
+		elem.mozRequestFullScreen();
+	} else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+		elem.webkitRequestFullscreen();
+	} else if (elem.msRequestFullscreen) { /* IE/Edge */
+		elem.msRequestFullscreen();
+	}
+	//e.stopPropagation();
+	return false;
+}
 
+/* Close fullscreen */
+function closeFullscreen(e) {
+	if (document.exitFullscreen) {
+		document.exitFullscreen();
+	} else if (document.mozCancelFullScreen) { /* Firefox */
+		document.mozCancelFullScreen();
+	} else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+		document.webkitExitFullscreen();
+	} else if (document.msExitFullscreen) { /* IE/Edge */
+		document.msExitFullscreen();
+	}
+	//e.stopPropagation();
+	return false;
+}
 
-	/* Get the documentElement (<html>) to display the page in fullscreen */
-	var elem = document.documentElement;
-
-	/* View in fullscreen */
-	function openFullscreen(e) {
-		if (elem.requestFullscreen) {
-			elem.requestFullscreen();
-		} else if (elem.mozRequestFullScreen) { /* Firefox */
-			elem.mozRequestFullScreen();
-		} else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-			elem.webkitRequestFullscreen();
-		} else if (elem.msRequestFullscreen) { /* IE/Edge */
-			elem.msRequestFullscreen();
-		}
+const cons = {
+	show: (e) => {
+		document.querySelector('.grid-container').style['grid-template-rows'] = 'auto 1fr 1px 300px';
+		//e.stopPropagation();
+		return false;
+	},
+	hide: (e) => {
+		document.querySelector('.grid-container')
+			.style['grid-template-rows'] = 'auto 1fr 1px 0px';
 		//e.stopPropagation();
 		return false;
 	}
-
-	/* Close fullscreen */
-	function closeFullscreen(e) {
-		if (document.exitFullscreen) {
-			document.exitFullscreen();
-		} else if (document.mozCancelFullScreen) { /* Firefox */
-			document.mozCancelFullScreen();
-		} else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
-			document.webkitExitFullscreen();
-		} else if (document.msExitFullscreen) { /* IE/Edge */
-			document.msExitFullscreen();
-		}
-		//e.stopPropagation();
-		return false;
-	}
-
-	const cons = {
-		show: (e) => {
-			document.querySelector('.grid-container').style['grid-template-rows'] = 'auto 1fr 1px 300px';
-			//e.stopPropagation();
-			return false;
-		},
-		hide: (e) => {
-			document.querySelector('.grid-container')
-				.style['grid-template-rows'] = 'auto 1fr 1px 0px';
-			//e.stopPropagation();
-			return false;
-		}
-	}
+}
 
 window.App = {
 	console: cons,
