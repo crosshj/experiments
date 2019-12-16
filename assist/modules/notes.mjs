d53db6ab47fc987fd9cbde4b9f4a01f8e9597b18
crosshj <github@chimpjuice.com>
2019-12-15 23:34:57 -0500

    assist: round out CRUD with update, delete - read single item

--- assist/modules/notes.mjs
+++ assist/modules/notes.mjs
@@ -5,6 +5,28 @@ function newUUID(){
 	return b();
 }
 
+// https://stackoverflow.com/questions/1125292/how-to-move-cursor-to-end-of-contenteditable-entity
+function setEndOfContenteditable(contentEditableElement)
+{
+    var range,selection;
+    if(document.createRange)//Firefox, Chrome, Opera, Safari, IE 9+
+    {
+        range = document.createRange();//Create a range (a range is a like the selection but invisible)
+        range.selectNodeContents(contentEditableElement);//Select the entire contents of the element with the range
+        range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+        selection = window.getSelection();//get the selection object (allows you to change selection)
+        selection.removeAllRanges();//remove any selections already made
+        selection.addRange(range);//make the range you have just created the visible selection
+    }
+    else if(document.selection)//IE 8 and lower
+    {
+        range = document.body.createTextRange();//Create a range (a range is a like the selection but invisible)
+        range.moveToElementText(contentEditableElement);//Select the entire contents of the element with the range
+        range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+        range.select();//Select the range (make it the visible selection
+    }
+}
+
 class Storage {
 	constructor(){
 		this.create = this.create.bind(this);
@@ -26,7 +48,14 @@ class Storage {
 		localStorage.setItem(`notes-${uuid}`, JSON.stringify(item));
 		return uuid;
 	}
-	read(){
+	read(key){
+		if(key){
+			let item = localStorage.getItem(`notes-${key}`);
+			try {
+				item = JSON.parse(item);
+			} catch(e){}
+			return item;
+		}
 		const index = JSON.parse(localStorage.getItem('notes-index')) || [];
 		const items = index.reduce((all, one) => {
 			let item = localStorage.getItem(`notes-${one}`);
@@ -40,11 +69,10 @@ class Storage {
 		}, []);
 		return items;
 	}
-	update(key){
-		console.log(`PLEASE UPDATE: ${key}`);
+	update(key, item){
+		localStorage.setItem(`notes-${key}`, JSON.stringify(item));
 	}
 	delete(key){
-		console.log(`PLEASE DELETE: ${key}`);
 		let index = JSON.parse(localStorage.getItem('notes-index')) || [];
 		index = index.filter(x => x !== key);
 		localStorage.setItem('notes-index', JSON.stringify(index));
@@ -83,6 +111,18 @@ function notesModule() {
 				<ul id="notes-list" class="collection"></ul>
 				<textarea rows=8 style="${taStyle}" spellcheck="false"></textarea>
 			</div>
+
+			<div id="modal-delete-confirm" class="modal">
+				<div class="modal-content">
+					<h4>Delete Note</h4>
+					<p>Sure you want to do this?</p>
+				</div>
+				<div class="modal-footer">
+					<a href="#!" class="modal-close waves-effect waves-red btn-flat">Cancel</a>
+					<a href="#!" class="modal-close waves-effect waves-green btn-flat">Okay</a>
+				</div>
+			</div>
+
 			<div class="flex-row center" style="${butRowStyle}">
 				<a class="add-button btn waves-effect waves-light">add</a>
 			</div>
@@ -91,15 +131,75 @@ function notesModule() {
 		const textBox = el.querySelector('textarea');
 		const notesList = el.querySelector('#notes-list');
 		const addButton = el.querySelector('.add-button');
+		const deleteConfirm = el.querySelector('#modal-delete-confirm');
+
+		var deleteConfirmModalEl;
+		var deleteConfirmModalInstance;
+
+		var deleteState;
+		deleteConfirm.onclick = (event) => {
+			const isCancel = event.target.outerHTML.includes('>Cancel<');
+			const isConfirm = event.target.outerHTML.includes('>Okay<');
+			if(isCancel){
+				deleteState = undefined;
+			}
+			if(deleteState && isConfirm){
+				storage.delete(deleteState.noteKey);
+				notesList.removeChild(deleteState.parent);
+				deleteState = undefined;
+			}
+		};
 
 		notesList.onclick = (event) => {
-			console.log(event.target);
+			const isButton = event.target.tagName.toLowerCase() === 'i';
 			const isDelete = event.target.outerHTML.includes('>clear<');
+			const isEdit = event.target.outerHTML.includes('>edit<');
+			const isEditConfirm = event.target.outerHTML.includes('>check<');
+			const isEditCancel = event.target.outerHTML.includes('>not_interested<');
+
+			if(!isButton){
+				return;
+			}
+			//console.log(event.target);
+
 			if(isDelete){
 				const parent = event.target.closest('li');
+				deleteState = {
+					parent,
+					noteKey: parent.dataset.key
+				};
+
+				deleteConfirmModalEl = deleteConfirmModalEl || el.querySelector('#modal-delete-confirm');
+				deleteConfirmModalInstance = deleteConfirmModalInstance || M.Modal.init(deleteConfirmModalEl);
+				deleteConfirmModalInstance.open();
+			}
+			if(isEdit){
+				const parent = event.target.closest('li');
+				parent.style.background = "rgba(255, 255, 255, 0.1)";
+				parent.querySelector('.actions:not(.edit)').classList.add('hidden');
+				parent.querySelector('.actions.edit').classList.remove('hidden');
+				parent.querySelector('span').setAttribute("contenteditable", true);
+				parent.querySelector('span').focus();
+				setEndOfContenteditable(parent.querySelector('span'));
+			}
+			if(isEditConfirm){
+				const parent = event.target.closest('li');
 				const noteKey = parent.dataset.key;
-				storage.delete(noteKey);
-				notesList.removeChild(parent);
+				parent.style.background = "";
+				const newText = parent.querySelector('span').innerHTML;
+				parent.querySelector('.actions:not(.edit)').classList.remove('hidden');
+				parent.querySelector('.actions.edit').classList.add('hidden');
+				parent.querySelector('span').setAttribute("contenteditable", false);
+				storage.update(noteKey, newText);
+			}
+			if(isEditCancel){
+				const parent = event.target.closest('li');
+				const noteKey = parent.dataset.key;
+				parent.style.background = "";
+				parent.querySelector('.actions:not(.edit)').classList.remove('hidden');
+				parent.querySelector('.actions.edit').classList.add('hidden');
+				parent.querySelector('span').setAttribute("contenteditable", false);
+				parent.querySelector('span').innerHTML = storage.read(noteKey);
 			}
 		};
 
@@ -107,7 +207,14 @@ function notesModule() {
 			notesList.innerHTML += `
 				<li class="collection-item" data-key="${key}">
 					<span>${item}</span>
-					<i class="material-icons">clear</i>
+					<div class="actions">
+						<i class="material-icons">edit</i>
+						<i class="material-icons">clear</i>
+					</div>
+					<div class="actions edit hidden">
+						<i class="material-icons">not_interested</i>
+						<i class="material-icons">check</i>
+					</div>
 				</li>
 			`;
 		};
