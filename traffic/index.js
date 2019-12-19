1088b873086cb0e03490b9211b535dca6517f9f0
crosshj <github@chimpjuice.com>
2019-12-18 21:46:56 -0500

    traffic-new to traffic

--- traffic/index.js
+++ traffic/index.js
@@ -1,25 +1,10 @@
 const startLoad = performance.now();
 
-import Editor from '../shared/modules/editor.mjs';
 import Sketch from './modules/sketch.mjs';
 import App from '../shared/modules/app.mjs';
 import AppDom from '../shared/modules/appDom.mjs';
+import Theme from '../shared/modules/theme.mjs';
 
-const editorText =
-`## current state:
-- basic project scaffolded, using sketch.js
-- some particle effects, but nothing like what is needed
-
-## todo:
-- get some basic particle system set up and make sure it can do what's needed: collision, spawn points, etc
-- create roads generatively
-- add vehicle particles
-- explore some ideas about traffic
-
-## resources:
-- https://news.ycombinator.com/item?id=6765029
-
-`;
 
 const appendStyleSheet = (url, callback) => {
 	var style = document.createElement('link');
@@ -29,6 +14,46 @@ const appendStyleSheet = (url, callback) => {
 	style.href = url;
 	document.head.appendChild(style);
 };
+
+// import Editor from '../shared/modules/Editor';
+// const editorText =
+// `## current state:
+// - basic project scaffolded, using sketch.js
+// - some particle effects, but nothing like what is needed
+
+// ## todo:
+// - get some basic particle system set up and make sure it can do what's needed: collision, spawn points, etc
+// - create roads generatively
+// - add vehicle particles
+// - explore some ideas about traffic
+
+// ## resources:
+// - https://news.ycombinator.com/item?id=6765029
+
+// `;
+
+window.Theme = Theme({});
+
+const opts = {
+	title: 'Traffic',
+	menu: {
+		"Actions": [{
+				text: "Toggle Dark",
+				onclick: "window.Theme.toggleDark()",
+				icon: "settings_brightness"
+			}, {
+				text: "Open Fullscreen",
+				onclick: "window.App.openFullscreen(event)",
+				icon: "fullscreen"
+			}, {
+				text: "Exit Fullscreen",
+				onclick: "window.App.closeFullscreen(event)",
+				icon: "fullscreen_exit"
+		}]
+	}
+};
+AppDom.config(opts);
+App.config(opts);
 
 AppDom(() => {
 	App((err, app) => {
@@ -41,14 +66,16 @@ AppDom(() => {
 				? timeToDelay - totalTime
 				: 0;
 
+			/*
+			Editor({ text: editorText }, (error, editor) => {
+				//TODO: error handle
+				window.Editor = editor;
+			});
+			*/
+
 			const loadingEl = document.querySelector('#loading-screen');
 			setTimeout(() => {
 				window.Sketch = Sketch();
-
-				Editor({ text: editorText }, (error, editor) => {
-					//TODO: error handle
-					window.Editor = editor;
-				});
 
 				loadingEl.classList.add('hidden');
 				document.body.classList.remove('loading');
