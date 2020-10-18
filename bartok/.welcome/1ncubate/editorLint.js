0e53ebff6b69d3a08d0642f8e4f2200209409a78
harrison.cross <harrison.cross@transport4.com>
2020-10-18 03:40:44 -0400

    bartok: editorLint++

--- bartok/.welcome/1ncubate/editorLint.js
+++ bartok/.welcome/1ncubate/editorLint.js
@@ -11,65 +11,64 @@ https://www.unpkg.com/browse/codemirror@5.58.1/addon/lint/
 */
 
 const deps = [
-    "../shared.styl",
-    "/shared/vendor/codemirror.js",
-    "https://www.unpkg.com/jshint@2.9.6/dist/jshint.js",
-    "https://www.unpkg.com/codemirror@5.58.1/addon/lint/lint.js",
-    "https://www.unpkg.com/codemirror@5.58.1/addon/lint/javascript-lint.js",
-    "https://www.unpkg.com/codemirror@5.58.1/addon/lint/lint.css",
-    "/shared/css/codemirror.css",
-    "/shared/css/vscode.codemirror.css",
-    "/shared/vendor/codemirror/mode.bundle.js"
-  ];
+  "../shared.styl",
+  "/shared/vendor/codemirror.js",
+  "https://www.unpkg.com/jshint@2.9.6/dist/jshint.js",
+  "https://www.unpkg.com/codemirror@5.58.1/addon/lint/lint.js",
+  "https://www.unpkg.com/codemirror@5.58.1/addon/lint/javascript-lint.js",
+  "https://www.unpkg.com/codemirror@5.58.1/addon/lint/lint.css",
+  "/shared/css/codemirror.css",
+  "/shared/css/vscode.codemirror.css",
+  "/shared/vendor/codemirror/mode.bundle.js"
+];
 
-  const dummyText = `
-  /*jshint esversion: 6 */
+const dummyText = `
+/*jshint esversion: 6 */
 
-  console.log('Hello, World');
-  console.log("Hello, World");
-  console.;
+console.log('Hello, World');
+console.log("Hello, World");
+console.;
 
-  function fred (){
+function fred (){
+}
 
-  }
+const foo = 'hello';
+foo = 'wow';
 
-  const foo = 'hello';
-  foo = 'wow';
+  var germ;
+for(var i=0; i<3;i++){
+  cons
+}
+`.trim()+'\r\n';
 
-    var germ;
-  for(var i=0; i<3;i++){
-    cons
-  }
-  `.trim()+'\r\n';
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-  function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
+const setupEditor = function(){
+  const editorDiv = document.createElement('div');
+  editorDiv.id = 'editor-div';
+  editorDiv.style.width = "100%";
+  editorDiv.style.height = "90vh";
+  editorDiv.style.border = "1px solid black";
+  document.body.appendChild(editorDiv);
 
-  const setupEditor = function(){
-    const editorDiv = document.createElement('div');
-    editorDiv.id = 'editor-div';
-    editorDiv.style.width = "100%";
-    editorDiv.style.height = "90vh";
-    editorDiv.style.border = "1px solid black";
-    document.body.appendChild(editorDiv);
+  const cm = CodeMirror(editorDiv, {
+    lineNumbers: true,
+    tabSize: 2,
+    mode: 'javascript',
+    theme: 'vscode-dark',
+    value: dummyText,
+    gutters: ["CodeMirror-lint-markers"],
+    lint: true
+  });
+  cm.setSize(null, 1000);
+};
 
-    const cm = CodeMirror(editorDiv, {
-      lineNumbers: true,
-      tabSize: 2,
-      mode: 'javascript',
-      theme: 'vscode-dark',
-      value: dummyText,
-      gutters: ["CodeMirror-lint-markers"],
-      lint: true
-    });
-    cm.setSize(null, 1000);
-  };
+(async () => {
+  await appendUrls(deps);
+  const editorDiv = setupEditor();
 
-  (async () => {
-    await appendUrls(deps);
-    const editorDiv = setupEditor();
-
-  })();
+})();
 
 
