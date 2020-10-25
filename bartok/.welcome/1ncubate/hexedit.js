101be8f8cf98970917be2cf8eb5da11208d89848
harrison.cross <harrison.cross@transport4.com>
2020-10-25 15:31:35 -0400

    bartok: woops from previous commits

--- bartok/.welcome/1ncubate/hexedit.js
+++ bartok/.welcome/1ncubate/hexedit.js
@@ -1,2 +1,20 @@
+/*
+
+https://github.com/thiscouldbebetter/HexEditor
 
 
+*/
+
+
+const deps = [
+	'../shared.styl'
+];
+
+(async () => {
+
+	await appendUrls(deps);
+
+	console.info('collecting references: see comments')
+
+
+})();
