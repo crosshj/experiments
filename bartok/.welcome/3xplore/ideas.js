19577e8c0b9338c07304495bf0fe5ec1aac4b507
harrison.cross <harrison.cross@transport4.com>
2020-11-23 01:19:30 -0500

    bartok: notes

---
+++ bartok/.welcome/3xplore/ideas.js
@@ -0,0 +1,53 @@
+/*
+
+Simulations:
+	N-copter
+		- quad copter
+		- copter with any given number of rotors
+		- real physics for rotors
+	biped - n-ped
+		- biped locomotion
+		- n-legged locomotion
+	wheeled vehicles kenematics
+		- https://robohub.org/pros-and-cons-for-different-types-of-drive-selection/
+		- differential drive robot
+		- ackerman steering (car)
+		- holonomic robot
+		- ball robot
+		- treaded
+
+Machine Learning:
+	- when should bank account be checked? (predict likliehood of change)
+
+Reinforcement Learning:
+	- wtf happened in my college class on this?
+
+Control Theory:
+	- PID tuning
+	- wtf happened in my college class on this?
+
+Signal Processing
+	- Kalman filter
+		- https://github.com/wouterbulten/kalmanjs
+		- https://github.com/infusion/Kalman.js/
+	- sensor fusion
+		- https://towardsdatascience.com/wtf-is-sensor-fusion-part-2-the-good-old-kalman-filter-3642f321440
+
+Image Processing and Manipulation
+	- object detection
+	- background removal
+	- chop and remix and shuffle an image
+	- basic operations I want to do all the time
+		- crop
+		- paste and add text or speech bubble
+		- graph of nodes with connections
+
+Game Helpers
+	Line Rangers
+		- whats the best ranger to upgrade?
+		- how often do certain rewards drop?
+		- is it worth it to purchase something?
+		- what is the best gear to place on ranger?
+
+
+*/
